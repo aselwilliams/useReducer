07976c03ab78a1b5769b1496acd53deede628419
aselwilliams <aselwilliams22@gmail.com>
2022-06-12 18:11:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { useState, useEffect} from 'react';
 import './App.css';
+import data from './data'
 
 function App() {
+  const [name, setName]=useState('')
+  const [people, setPeople] =useState(data);
+  const [showModal, setShowModal] =useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {showModal && <Modal />}
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input type='text' value={name} onChange={(e)=>setName(e.target.value)} />
+      </div>
+
+    </form>
+    </>
   );
 }
 
