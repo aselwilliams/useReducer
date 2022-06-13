b3a407a66283b807496c0162118f3eb694fd9694
aselwilliams <aselwilliams22@gmail.com>
2022-06-12 20:08:13 -0400

    separate file added

--- src/App.js
+++ src/App.js
@@ -2,36 +2,7 @@ import { useState, useEffect, useReducer } from "react";
 import "./App.css";
 import { data } from "./data";
 import Modal from "./Modal";
-
-const reducer = (state, action) => {
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.payload];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "Person Added",
-    };
-  }
-  if (action.type === "NO_VALUE") {
-    return { ...state, isModalOpen: true, modalContent: "Please enter value" };
-  }
-  if (action.type === "CLOSE_MODAL") {
-    return { ...state, isModalOpen: false };
-  }
-  if (action.type === "REMOVE_ITEM") {
-    const newPeople = state.people.filter(
-      (person) => person.id !== action.payload
-    );
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "Person Removed",
-    };
-  }
-  throw new Error("no matching action type");
-};
+import {reducer} from './reducer'
 
 const defaultState = {
   people: [],
